72a375078ef5bd747403a8203a9bfe37f48a93c9
twdtraderslab-oss <twdtraderslab@gmail.com>
2026-06-11 21:59:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11', 'Chrome >= 60', 'Android >= 6'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-      modernPolyfills: true,
-    })
-  ],
+  plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
     target: 'es2015',
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          charts: ['recharts'],
-          supabase: ['@supabase/supabase-js'],
+          'vendor':   ['react', 'react-dom'],
+          'supabase': ['@supabase/supabase-js'],
+          'charts':   ['recharts'],
         }
       }
     }
